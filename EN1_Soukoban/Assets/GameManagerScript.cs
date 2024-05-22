33b6efdc8cd9b2e2c318c97e331387e03923ab87
tsubasak1217 <k023g0112@g.neec.ac.jp>
2024-05-22 17:15:04 +0900

    save

--- EN1_Soukoban/Assets/GameManagerScript.cs
+++ EN1_Soukoban/Assets/GameManagerScript.cs
@@ -1,113 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class GameManagerScript : MonoBehaviour
-{
+public class GameManagerScript : MonoBehaviour {
 
-    int[] map = { 0, 2, 0, 1, 0, 2, 0, 0, 0 };
+	int[,] map;
+	public GameObject PlayerPrefab;
+	GameObject[,] field;
+	int blockSize;
+	Vector2Int playerIndex = new Vector2Int(-1, -1);
+	string debugText = "";
+
+	// Start is called before the first frame update
+	void Start() {
+
+		map = new int[,] {
+			{ 1, 0, 2, 2, 0, 0, 2, 0, 2, 2, 0, 0 },
+			{ 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 }
+		};
+
+		blockSize = 64;
+
+		field = new GameObject[
+			map.GetLength(0),
+			map.GetLength(1)
+			];
+
+		for(int i = 0; i < map.GetLength(0); i++) {
+			for(int j = 0; j < map.GetLength(1); j++) {
+				if(map[i, j] == 1) {
+					field[i, j] = Instantiate(
+						PlayerPrefab,
+						new Vector3(j, map.GetLength(0) - i, 0),
+						Quaternion.identity
+					);
+				}
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+		if(Input.GetKeyDown(KeyCode.RightArrow)) {
+			// プレイヤーのいる座標を取得
+			playerIndex = GetPlayerIndex();
+
+			//移動
+			MoveNum(playerIndex, playerIndex + new Vector2Int(1, 0));
+
+			// 更新した結果を出力
+			OutputLog();
+		} else if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			// プレイヤーのいる座標を取得
+			playerIndex = GetPlayerIndex();
+
+			//移動
+			MoveNum(playerIndex, playerIndex + new Vector2Int(-1, 0));
+
+			// 更新した結果を出力
+			OutputLog();
+
+		} else if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			// プレイヤーのいる座標を取得
+			playerIndex = GetPlayerIndex();
+
+			//移動
+			MoveNum(playerIndex, playerIndex + new Vector2Int(0, -1));
+
+			// 更新した結果を出力
+			OutputLog();
+
+		} else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			// プレイヤーのいる座標を取得
+			playerIndex = GetPlayerIndex();
+
+			//移動
+			MoveNum(playerIndex, playerIndex + new Vector2Int(0, 1));
+
+			// 更新した結果を出力
+			OutputLog();
+		}
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
+		/*field = new GameObject[
+			map.GetLength(0),
+			map.GetLength(1)
+			];*/
 
-    }
+		/*for(int i = 0; i < map.GetLength(0); i++) {
+			for(int j = 0; j < map.GetLength(1); j++) {
+				if(map[i, j] == 1) {
+					field[i, j] = Instantiate(
+					   PlayerPrefab,
+						new Vector3(j, map.GetLength(0) - i, 0),
+						Quaternion.identity
+					);
+				}
+			}
+		}*/
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        int playerIndex = -1;
-        string debugText = "";
+	// 更新した結果を出力する関数
+	void OutputLog() {
+		for(int i = 0; i < map.GetLength(0); i++) {
+			debugText += "\n";
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            // プレイヤーのいる座標を取得
-            playerIndex = GetPlayerIndex(map);
+			for(int j = 0; j < map.GetLength(1); j++) {
+				debugText += map[i, j].ToString();
+			}
+		}
 
-            // プレイヤーの進む先が配列の範囲内か
-            if (playerIndex + 1 <= map.Length)
-            {
-                // 進む先に箱があるか
-                if (map[playerIndex + 1] == 2)
-                {
-                    // 箱のひとつ先が空いているか
-                    if (playerIndex + 2 < map.Length)
-                    {
-                        MoveNum(map[playerIndex + 1], map[playerIndex + 2]);
-                        MoveNum(map[playerIndex], map[playerIndex + 1]);
-                    }
-                }
-                else// 箱がないとき
-                {
-                    MoveNum(map[playerIndex], map[playerIndex + 1]);
-                }
-            }
+		Debug.Log(debugText);
+		debugText = "";
+	}
 
-            OutputLog();
+	// 数値を移動する関数
+	bool MoveNum(Vector2Int startIdx, Vector2Int goalIdx) {
 
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            // プレイヤーのいる座標を取得
-            playerIndex = GetPlayerIndex(map);
+		if(goalIdx.x < 0 || goalIdx.x >= field.GetLength(1)) { return false; }
+		if(goalIdx.y < 0 || goalIdx.y >= field.GetLength(0)) { return false; }
 
-            // プレイヤーの進む先が配列の範囲内か
-            if (playerIndex - 1 >= 0)
-            {
-                // 進む先に箱があるか
-                if (map[playerIndex - 1] == 2)
-                {
-                    // 箱のひとつ先が空いているか
-                    if (playerIndex - 2 >= 0)
-                    {
-                        MoveNum(map[playerIndex - 1], map[playerIndex - 2]);
-                        MoveNum(map[playerIndex], map[playerIndex - 1]);
-                    }
-                }
-                else // 箱がないとき
-                {
-                    MoveNum(map[playerIndex], map[playerIndex - 1]);
-                }
-            }
+		Vector2Int direction = goalIdx - startIdx;
+		Vector2Int tmp = direction;
+		int count = 1;
 
-            // 更新した結果を出力
-            OutputLog();
-        }
+		// 移動先が箱じゃなくなるまでループ
+		while(map[startIdx.y + tmp.y, startIdx.x + tmp.x] == 2) {
+			tmp += direction;
+			count++;
 
-        // 更新した結果を出力する関数
-        void OutputLog()
-        {
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString();
-            }
+			// 範囲外に出た場合は移動できない
+			if(startIdx.x + tmp.x < 0 || startIdx.x + tmp.x >= field.GetLength(1)) { return false; }
+			if(startIdx.y + tmp.y < 0 || startIdx.y + tmp.y >= field.GetLength(0)) { return false; }
+		}
 
-            Debug.Log(debugText);
-        }
+		for(int i = count; i > 0; i--) {
+			map[startIdx.y + tmp.y, startIdx.x + tmp.x]
+				= map[startIdx.y + tmp.y - direction.y, startIdx.x + tmp.x - direction.x];//マップ番号の移動
+			map[startIdx.y + tmp.y - direction.y, startIdx.x + tmp.x - direction.x] = 0;// 空いた穴を0にする
 
-        // 数値を移動する関数
-        void MoveNum(int startIdx,int goalIdx)
-        {
-            goalIdx = startIdx;
-            startIdx = 0;
-        }
-    }
+			field[startIdx.y + tmp.y, startIdx.x + tmp.x]
+				= field[startIdx.y + tmp.y - direction.y, startIdx.x + tmp.x - direction.x];//マップ番号の移動
+			field[startIdx.y + tmp.y - direction.y, startIdx.x + tmp.x - direction.x] = null;// 空いた穴を0にする
 
-    // プレイヤーのいる座標を取得する関数
-    int GetPlayerIndex(int[] array)
-    {
-        int result = -1;
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i] == 1)
-            {
-                result = i;
-                break;
-            }
-        }
+			field[startIdx.y + tmp.y, startIdx.x + tmp.x].transform.position =
+				new Vector3(startIdx.x + tmp.x, map.GetLength(0) - startIdx.y + tmp.y, 0);
 
-        return result;
-    }
+			tmp -= direction;
+		}
+
+		return true;
+	}
+
+	// プレイヤーのいる座標を取得する関数
+	Vector2Int GetPlayerIndex() {
+
+		for(int i = 0; i < field.GetLength(0); i++) {
+			for(int j = 0; j < field.GetLength(1); j++) {
+
+				if(field[i, j] == null) { continue; }
+
+				if(field[i, j].tag == "Player") {
+					return new Vector2Int(j, i);
+				}
+			}
+		}
+
+		return new Vector2Int(-1, -1);
+	}
 
 }
